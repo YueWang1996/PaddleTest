ab93e93a100d973642b69560535e9566a6ee3d63
YueWang1996 <YueWang1996@users.noreply.github.com>
2021-01-20 15:54:39 +0100

    Update test.py
    https://github.com/PaddlePaddle/book/blob/develop/01.fit_a_line/train.py

--- test.py
+++ test.py
@@ -41,18 +41,18 @@ def parse_args():
     return args
 
 
-# For training test cost 创建训练过程
+# For training test cost
 def train_test(executor, program, reader, feeder, fetch_list):
     accumulated = 1 * [0]
     count = 0
     for data_test in reader():
         outs = executor.run(
             program=program, feed=feeder.feed(data_test), fetch_list=fetch_list)
-        accumulated = [x_c[0] + x_c[1][0] for x_c in zip(accumulated, outs)] # 累加测试过程中的损失值
-        count += 1 # 累加测试集中的样本数量
-    return [x_d / count for x_d in accumulated] # 计算平均损失
+        accumulated = [x_c[0] + x_c[1][0] for x_c in zip(accumulated, outs)]
+        count += 1
+    return [x_d / count for x_d in accumulated]
 
-# 预测: 保存图片
+
 def save_result(points1, points2):
     import matplotlib
     matplotlib.use('Agg')
@@ -68,8 +68,8 @@ def save_result(points1, points2):
     plt.savefig('./image/prediction_gt.png')
 
 
-def main(): 
-    batch_size = 20 #configure data provider 配置数据提供器(Datafeeder)
+def main():
+    batch_size = 20
 
     if args.enable_ce:
         train_reader = paddle.batch(
@@ -86,27 +86,27 @@ def main():
                 paddle.dataset.uci_housing.test(), buf_size=500),
             batch_size=batch_size)
 
-    # feature vector of length 13 #configure training program 配置训练程序
-    x = fluid.data(name='x', shape=[None, 13], dtype='float32')  # 定义输入的形状和数据类型
-    y = fluid.data(name='y', shape=[None, 1], dtype='float32') # 定义输出的形状和数据类型
+    # feature vector of length 13
+    x = fluid.data(name='x', shape=[None, 13], dtype='float32')
+    y = fluid.data(name='y', shape=[None, 1], dtype='float32')
 
-    main_program = fluid.default_main_program() # 获取默认/全局主函数
-    startup_program = fluid.default_startup_program() # 获取默认/全局启动程序
+    main_program = fluid.default_main_program()
+    startup_program = fluid.default_startup_program()
 
     if args.enable_ce:
         main_program.random_seed = 90
         startup_program.random_seed = 90
 
     y_predict = fluid.layers.fc(input=x, size=1, act=None)
-    cost = fluid.layers.square_error_cost(input=y_predict, label=y) # 利用标签数据和输出的预测数据估计方差
-    avg_loss = fluid.layers.mean(cost) # 对方差求均值，得到平均损失
+    cost = fluid.layers.square_error_cost(input=y_predict, label=y)
+    avg_loss = fluid.layers.mean(cost)
 
-    test_program = main_program.clone(for_test=True) # 克隆main_program得到test_program
+    test_program = main_program.clone(for_test=True)
 
-    sgd_optimizer = fluid.optimizer.SGD(learning_rate=0.001) # learning_rate 是学习率 # 选择优化方法
+    sgd_optimizer = fluid.optimizer.SGD(learning_rate=0.001)
     sgd_optimizer.minimize(avg_loss)
 
-    # can use CPU or GPU # 网络参数初始化
+    # can use CPU or GPU
     use_cuda = args.use_gpu
     place = fluid.CUDAPlace(0) if use_cuda else fluid.CPUPlace()
     exe = fluid.Executor(place)
@@ -115,7 +115,7 @@ def main():
     params_dirname = "fit_a_line.inference.model"
     num_epochs = args.num_epochs
 
-    # main train loop. # 给出需要存储的目录名，并初始化一个执行器
+    # main train loop.
     feeder = fluid.DataFeeder(place=place, feed_list=[x, y])
     exe.run(startup_program)
 
@@ -124,18 +124,18 @@ def main():
     step = 0
 
     exe_test = fluid.Executor(place)
-    # 设置训练主循环
+
     for pass_id in range(num_epochs):
         for data_train in train_reader():
             avg_loss_value, = exe.run(
                 main_program,
                 feed=feeder.feed(data_train),
                 fetch_list=[avg_loss])
-            if step % 10 == 0:  # record a train cost every 10 batches  每10个批次记录并输出一下训练损失
+            if step % 10 == 0:  # record a train cost every 10 batches
                 print("%s, Step %d, Cost %f" %
                       (train_prompt, step, avg_loss_value[0]))
 
-            if step % 100 == 0:  # record a test cost every 100 batches 每100批次记录并输出一下测试损失
+            if step % 100 == 0:  # record a test cost every 100 batches
                 test_metics = train_test(
                     executor=exe_test,
                     program=test_program,
@@ -144,7 +144,7 @@ def main():
                     feeder=feeder)
                 print("%s, Step %d, Cost %f" %
                       (test_prompt, step, test_metics[0]))
-                # If the accuracy is good enough, we can stop the training. 如果准确率达到要求，则停止训练
+                # If the accuracy is good enough, we can stop the training.
                 if test_metics[0] < 10.0:
                     break
 
@@ -153,47 +153,47 @@ def main():
             if math.isnan(float(avg_loss_value[0])):
                 sys.exit("got NaN loss, training failed.")
         if params_dirname is not None:
-            # We can save the trained parameters for the inferences later  保存训练参数到之前给定的路径中
+            # We can save the trained parameters for the inferences later
             fluid.io.save_inference_model(params_dirname, ['x'], [y_predict],
                                           exe)
 
         if args.enable_ce and pass_id == args.num_epochs - 1:
             print("kpis\ttrain_cost\t%f" % avg_loss_value[0])
             print("kpis\ttest_cost\t%f" % test_metics[0])
-    # 准备预测环境
+
     infer_exe = fluid.Executor(place)
     inference_scope = fluid.core.Scope()
 
-    # infer 预测：通过fluid.io.load_inference_model，预测器会从params_dirname中读取已经训练好的模型，来对从未遇见过的数据进行预测
+    # infer
     with fluid.scope_guard(inference_scope):
         [inference_program, feed_target_names, fetch_targets
-         ] = fluid.io.load_inference_model(params_dirname, infer_exe)  # 载入预训练模型
+         ] = fluid.io.load_inference_model(params_dirname, infer_exe)
         batch_size = 10
 
         infer_reader = paddle.batch(
-            paddle.dataset.uci_housing.test(), batch_size=batch_size) # 准备测试集
+            paddle.dataset.uci_housing.test(), batch_size=batch_size)
 
         infer_data = next(infer_reader())
         infer_feat = numpy.array(
-            [data[0] for data in infer_data]).astype("float32") # 提取测试集中的数据
+            [data[0] for data in infer_data]).astype("float32")
         infer_label = numpy.array(
-            [data[1] for data in infer_data]).astype("float32") # 提取测试集中的标签
+            [data[1] for data in infer_data]).astype("float32")
 
         assert feed_target_names[0] == 'x'
         results = infer_exe.run(
             inference_program,
-            feed={feed_target_names[0]: numpy.array(infer_feat)}, 
-            fetch_list=fetch_targets) # 进行预测
-        #打印预测结果和标签并可视化结果
+            feed={feed_target_names[0]: numpy.array(infer_feat)},
+            fetch_list=fetch_targets)
+
         print("infer results: (House Price)")
         for idx, val in enumerate(results[0]):
-            print("%d: %.2f" % (idx, val)) # 打印预测结果
+            print("%d: %.2f" % (idx, val))
 
         print("\nground truth:")
         for idx, val in enumerate(infer_label):
-            print("%d: %.2f" % (idx, val)) # 打印标签值
+            print("%d: %.2f" % (idx, val))
 
-        save_result(results[0], infer_label) # 保存图片
+        save_result(results[0], infer_label)
 
 
 if __name__ == '__main__':
